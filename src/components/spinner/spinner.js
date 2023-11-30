5a7f85df1f0d3b3d0d98d637a62be46c4bbe8a5c
yusennn <116973490+yusennn@users.noreply.github.com>
2023-11-30 09:48:08 +0600

    Spinner was created

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css';
+
+export default () => {
+    return (
+        <div className="lds-css">
+            <div className="lds-double-ring">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
