ba28aa8a60fe3afc4217472ad01c510725170960
yusennn <116973490+yusennn@users.noreply.github.com>
2023-11-30 10:27:10 +0600

    Commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,59 +1,88 @@
+import React, {Component} from 'react';
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ErrorButton from '../error-button';
+import Row from "../row/row";
+import ErrorIndicator from "../error-indicator";
+
+import './app.css';
+
+import PeoplePage from "../people-page";
+import ItemList from "../item-list";
+import ItemDetails, {Record} from "../item-details/item-details";
+import SwapiService from "../../services/swapi-service";
+import ErrorBoundry from "../error-boundry";
+
+
+
 export default class App extends Component{
     swapiService = new SwapiService();
+
     state = {
-        showRandomPlanet: true,
-        hasError: false
+        showRandomPlanet: true
     };
+
+
     toggleRandomPlanet = () => {
         this.setState((state)=> {
             return { showRandomPlanet: !state.showRandomPlanet }
         })
     };
-    componentDidCatch() {
-        this.setState({ hasError: true });
-    }
-    render() {
-        if(this.state.hasError) return <ErrorIndicator />;
-        const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-        return (
-            <div className='container-fluid'>
-                <div className="row">
-                    <div className="col-12">
-                        <Header/>
-                        {planet}
-                        <button className="toggle-planet btn btn-warning btn-lg" onClick={this.toggleRandomPlanet}>
-                            Toggle Random Planet
-                        </button>
-                        <ErrorButton />
-                    </div>
-                </div>
-                <PeoplePage />
 
-                {/*<div className="row">
-                    <div className="col-md-6">
-                        <ItemList
-                            onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllPlanets}
-                            renderItem={  (item) => <span>{item.name} <button>!</button></span>}
-                        />
+
+    render() {
+        const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+
+        const { getPerson,
+            getStarship,
+            getPersonImage,
+            getStarshipImage,
+            getAllPeople,
+            getAllPlanets } = this.swapiService;
+
+        const personDetails = <ItemDetails
+            itemId={11}
+            getData={getPerson}
+            getImageUrl={getPersonImage}
+        >
+            <Record field="gender" label="Gender"/>
+            <Record field="eyeColor" label="Eye Color"/>
+        </ItemDetails>;
+        const starshipDetails = <ItemDetails
+            itemId={5}
+            getData={getStarship}
+            getImageUrl={getStarshipImage}
+        >
+            <Record field="model" label="Model"/>
+            <Record field="length" label="Length"/>
+            <Record field="costInCredits" label="Cost"/>
+        </ItemDetails>;
+
+        return (
+            <ErrorBoundry>
+                <div className='container-fluid'>
+                    <div className="row">
+                        <div className="col-12">
+                            <Header/>
+                        </div>
                     </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson} />
-                    </div>
+
+
+                    <ItemList
+                        getData={getAllPeople}
+                        onItemSelected={() => {}}>
+
+                        { ({name}) => <span>{name}</span> }
+                    </ItemList>
+
+                    <ItemList
+                        getData={getAllPlanets}
+                        onItemSelected={() => {}}>
+
+                        { ({name}) => <span>{name}</span> }
+                    </ItemList>
                 </div>
-                <div className="row">
-                    <div className="col-md-6">
-                        <ItemList
-                            onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllStarships}
-                            renderItem={  ({ name, gender, birthYear }) => `${name} 5`}
-                        />
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson} />
-                    </div>
-                </div>*/}
-            </div>
+            </ErrorBoundry>
         );
     }
 };
--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,45 +1,44 @@
 import React, {Component} from 'react';
 import './people-page.css';
 import ItemList from '../item-list';
-import PersonDetails from "../person-details";
-import ErrorIndicator from "../error-indicator";
+import ItemDetails from "../item-details";
 import SwapiService from "../../services/swapi-service";
 import Row from "../row";
-import ErrorButton from "../error-button";
 import ErrorBoundry from "../error-boundry";
+
 
 
 export default class PeoplePage extends Component {
     swapiService = new SwapiService();
+
     state = {
         selectedPerson: 3,
-        hasError: false
     };
-
 
 
     onPersonSelected = (selectedPerson) => {
         this.setState({ selectedPerson })
     };
-    render() {
-        if(this.state.hasError) return <ErrorIndicator />;
-        const itemList = (<ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-        >
-            { (i) => `${i.name} (${i.birthYear})}` }
-        </ItemList>);
 
-        const personDetails = (
+
+    render() {
+        const itemList = (
+            <ItemList
+                onItemSelected={this.onPersonSelected}
+                getData={this.swapiService.getAllPeople}
+            >
+                { (i) => `${i.name} (${i.birthYear})` }
+            </ItemList>
+        );
+
+        const itemDetails = (
             <ErrorBoundry>
-            <PersonDetails personId={this.state.selectedPerson} />
+                <ItemDetails itemId={this.state.selectedPerson} />
             </ErrorBoundry>
-            );
+        );
 
         return (
-
-                <Row left={itemList} right={personDetails} />
-
+            <Row left={itemList} right={itemDetails} />
         )
     }
 }
