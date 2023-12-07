72e9c8a6bdf6095c9630558658bd623dc98d1b80
yusennn <116973490+yusennn@users.noreply.github.com>
2023-12-07 06:17:26 +0600

    I have added Context for SwapiService

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,11 +8,17 @@ import './app.css';
 import PeoplePage from "../people-page";
 import ItemList from "../item-list";
 import ItemDetails, {Record} from "../item-details/item-details";
-import SwapiService from "../../services/swapi-service";
 import ErrorBoundry from "../error-boundry";
 import {PersonList, PlanetList, StarshipList, PersonDetails, StarshipDetails, PlanetDetails} from "../sw-components";
+import SwapiService from "../../services/swapi-service";
+import DummySwapiService from "../../services/dummy-swapi-service";
+import {SwapiServiceProvider} from "../swapi-service-context";
+
+
+
 export default class App extends Component{
-    swapiService = new SwapiService();
+    swapiService = new DummySwapiService();
+
     state = {
         showRandomPlanet: true
     };
@@ -21,53 +27,28 @@ export default class App extends Component{
             return { showRandomPlanet: !state.showRandomPlanet }
         })
     };
+
+
     render() {
-        const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-        /*const { getPerson,
-            getStarship,
-            getPersonImage,
-            getStarshipImage,
-            getAllPeople,
-            getAllPlanets } = this.swapiService;*/
-        /*const personDetails = <ItemDetails
-            itemId={11}
-            getData={getPerson}
-            getImageUrl={getPersonImage}
-        >
-            <Record field="gender" label="Gender"/>
-            <Record field="eyeColor" label="Eye Color"/>
-        </ItemDetails>;
-        const starshipDetails = <ItemDetails
-            itemId={5}
-            getData={getStarship}
-            getImageUrl={getStarshipImage}
-        >
-            <Record field="model" label="Model"/>
-            <Record field="length" label="Length"/>
-            <Record field="costInCredits" label="Cost"/>
-        </ItemDetails>;*/
         return (
             <ErrorBoundry>
-                <div className='container-fluid'>
-                    <div className="row">
-                        <div className="col-12">
-                            <Header/>
+                <SwapiServiceProvider value={this.swapiService}>
+                    <div className='container-fluid'>
+                        <div className="row">
+                            <div className="col-12">
+                                <Header/>
+                            </div>
                         </div>
+
+                        <PersonDetails itemId={11} />
+                        <StarshipDetails itemId={5} />
+                        <PlanetDetails itemId={9} />
+
+                        <PersonList />
+                        <StarshipList />
+                        <PlanetList />
                     </div>
-                    <PersonDetails itemId={11} />
-                    <StarshipDetails itemId={5} />
-                    <PlanetDetails itemId={9} />
-
-                    <PersonList />
-                    <StarshipList />
-                    <PlanetList />
-
-                    {/*<ItemList
-                        getData={getAllPlanets}
-                        onItemSelected={() => {}}>
-                        { ({name}) => <span>{name}</span> }
-                    </ItemList>*/}
-                </div>
+                </SwapiServiceProvider>
             </ErrorBoundry>
         );
     }
---
+++ src/components/swapi-service-context/index.js
@@ -0,0 +1,2 @@
+import {SwapiServiceProvider, SwapiServiceConsumer} from './swapi-service-context';
+export {SwapiServiceProvider, SwapiServiceConsumer};
